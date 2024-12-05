ceda247eeeb10873a901aaad528d7149ef400338
Dias <110160920+diasdauletov@users.noreply.github.com>
2024-12-05 01:03:49 +0000

    More elegant solution

--- 2024/04.cpp
+++ 2024/04.cpp
@@ -1,58 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool horizontalMatch(int row, int col, string & word, vector<string> & grid) {
-    for (char ch : word) {
-        if (col >= grid[row].size() || grid[row][col] != ch)
-            return false;
-        col++;
-    }
-    return true;
-}
-
-bool verticalMatch(int row, int col, string & word, vector<string> & grid) {
-    for (char ch : word) {
-        if (row >= grid.size() || grid[row][col] != ch)
-            return false;
-        row++;
-    }
-    return true;
-}
-
-int diagonalMatchCount(int startRow, int startCol, string & word, vector<string> & grid) {
-    // direction: right down
-    int rightDownMatch = 1;
-    int row = startRow, col = startCol;
-    for (char ch : word) {
-        if (row >= grid.size() || col >= grid[row].size() || grid[row][col] != ch) {
-            rightDownMatch = 0;
-            break;
-        }
-        row++; col++;
-    }
-    // direction: left down
-    int leftDownMatch = 1;
-    row = startRow;
-    col = startCol;
-    for (char ch : word) {
-        if (row < 0 || col >= grid[row].size() || grid[row][col] != ch) {
-            leftDownMatch = 0;
-            break;
-        }
-        row--; col++;
-    }
-    return rightDownMatch + leftDownMatch;
+bool matches(int dx, int dy, int x, int y, string word, vector<string> & grid) {
+    if (word.empty())
+        return true;
+    if (x < 0 || x >= grid.size() || y < 0 || y >= grid[x].size())
+        return false;
+    return word[0] == grid[x][y] && matches(dx, dy, x + dx, y + dy, word.substr(1), grid);
 }
 
 int searchWordInGrid(string word, vector<string> & grid) {
     int matchCount = 0;
     for (int i = 0; i < grid.size(); i++) {
         for (int j = 0; j < grid[i].size(); j++) {
-            if (horizontalMatch(i, j, word, grid))
-                matchCount++;
-            if (verticalMatch(i, j, word, grid))
-                matchCount++;
-            matchCount += diagonalMatchCount(i, j, word, grid);
+            // search in 8 directions from the center: + and x
+            vector<pair<int, int>> dirs = {
+                {0, 1}, {0, -1}, {1, 0}, {-1, 0}, // right, left, down, up
+                {1, 1}, {1, -1}, {-1, 1}, {-1, -1} // diagonal directions
+            };
+            for (auto [dx, dy] : dirs) {
+                matchCount += matches(dx, dy, i, j, word, grid);
+            }
         }
     }
     return matchCount;
@@ -67,9 +35,7 @@ int main() {
         grid.push_back(inputLine);        
     }
 
-    int res = searchWordInGrid("XMAS", grid) + searchWordInGrid("SAMX", grid);
-
     ofstream output("output/04.txt");
     cout.rdbuf(output.rdbuf());
-    cout << res;
+    cout << searchWordInGrid("XMAS", grid);
 }
